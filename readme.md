65f189ac6e6162ca5709d08e65dd0ad17eb78211
elsehow <ffff@ffffs-MacBook-Air.local>
2015-06-02 12:35:52 -0700

    refactored, added a config file

--- readme.md
+++ readme.md
@@ -1,5 +1,20 @@
 ## indra collection server
 
+this server takes post requests with the schema:
+```
+{
+  user: 'someString'
+  device: 'someString'
+  data: {arbitrary: 'object', ... }
+}
+```
+
+and posts them over the pusher channel defined in config.js
+
+## development
+
 first, `npm install`
 
 to run, `node server.js | bunyan`
+
+for tests, refer to the [e2e test suite](http://github.com/testing-suite)
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var restify = require('restify')
 var bunyan = require('bunyan')
 var log = bunyan.createLogger({name: 'indra-collection-server'});
 var isValid = require('./indraSchemaValidator.js')
+var config = require('./config.js')
 
 // config
 var port = 23023
@@ -13,19 +14,18 @@ server.use(restify.fullResponse())
 
 function handleRequest (req, res, next) {
   if (isValid(req.body) && req.params.channel) {
-    log.info('channel -> %s', req.params.channel)
     res.send(201)
     return next();
   }
   else {
     log.warn('bad schema -> %s', JSON.stringify(req.body));
-    return next(new restify.UnprocessableEntityError('API requests must be json, with proper headers, and must be posted to a channel. Refer to the API guide for JSON schema.'));
+    return next(new restify.UnprocessableEntityError(config.unprocessableEntityErrorMessage));
   }
   // log.warn('error saving data -> %s', err);
-  // return next(new restify.BadRequestError('API requests must be json, with proper headers.'));
+  // return next(new restify.BadRequestError(config.badRequestErrorMessage));
 }
 
-// TODO: lazily coded 'rooms'
+// lazily coded 'channels'
 server.post('/post/:channel', handleRequest)
 
 server.listen(port)

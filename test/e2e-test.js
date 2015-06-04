07a88fcb1c74c2f81b307c1eaff453f8601aabb4
elsehow <ffff@ffffs-MacBook-Air.local>
2015-06-04 08:32:25 -0700

    changed tabs in tests to spaces

--- test/e2e-test.js
+++ test/e2e-test.js
@@ -11,12 +11,12 @@ var PusherClient = require('pusher-client')
  
 // json REST client
 var jsonRestClient = request.createClient(
-	// URL OF RUNNING INDRA SERVER HERE
-	'http://indra.webfactional.com')
+  // URL OF RUNNING INDRA SERVER HERE
+  'http://indra.webfactional.com')
 
 // pusher socket client
 var indraChannel = new PusherClient(config.PUSHER_KEY
-	, {encrypted: config.IS_PUSHER_ENCRYPTED}).subscribe('everything');
+  , {encrypted: config.IS_PUSHER_ENCRYPTED}).subscribe('everything');
 
 // couchdb
 var cradle = require('cradle')
@@ -34,51 +34,51 @@ var goodData = {
 }
 
 var badData = {
-	user: 'this part is fine',
-	device: 'oops theres no type field',
-	data: {thisPart:'is fine'}
+  user: 'this part is fine',
+  device: 'oops theres no type field',
+  data: {thisPart:'is fine'}
 }
 
 test('posting good data -> 202', function(t) {
-	t.plan(1)
-	jsonRestClient.post(
-		'/',
-		goodData,
-		function(error, response, body) {
-			t.equal(202, response.statusCode)
-		}
-	)
+  t.plan(1)
+  jsonRestClient.post(
+    '/',
+    goodData,
+    function(error, response, body) {
+      t.equal(202, response.statusCode)
+    }
+  )
 })
 
 test('posting bad data -> 422 + UnprocessableEntityError', function(t) {
-	t.plan(6)
-	// 1. poorly-formed data
-	jsonRestClient.post(
-		'/',
-		badData,
-		function(error, response, body) {
-			t.equal('UnprocessableEntityError', body.code, 'null data UnprocessableEntityError')
-			t.equal(422, response.statusCode, 'bad data 422')
-		}
-	)
-	// 2. null data
-	jsonRestClient.post(
-		'/', 
-		null,
-		function(error, response, body) {
-			t.equal('UnprocessableEntityError', body.code, 'null data UnprocessableEntityError')
-			t.equal(422, response.statusCode, 'null data 422')
-		}
-	)
-	// 3. empty data
-	jsonRestClient.post(
-		'/', 
-		{},
-		function(error, response, body) {
-			t.equal('UnprocessableEntityError', body.code, 'empty data UnprocessableEntityError')
-			t.equal(422, response.statusCode, 'empty data 422')
-		}
-	)
+  t.plan(6)
+  // 1. poorly-formed data
+  jsonRestClient.post(
+    '/',
+    badData,
+    function(error, response, body) {
+      t.equal('UnprocessableEntityError', body.code, 'null data UnprocessableEntityError')
+      t.equal(422, response.statusCode, 'bad data 422')
+    }
+  )
+  // 2. null data
+  jsonRestClient.post(
+    '/', 
+    null,
+    function(error, response, body) {
+      t.equal('UnprocessableEntityError', body.code, 'null data UnprocessableEntityError')
+      t.equal(422, response.statusCode, 'null data 422')
+    }
+  )
+  // 3. empty data
+  jsonRestClient.post(
+    '/', 
+    {},
+    function(error, response, body) {
+      t.equal('UnprocessableEntityError', body.code, 'empty data UnprocessableEntityError')
+      t.equal(422, response.statusCode, 'empty data 422')
+    }
+  )
 })
 
 
@@ -88,50 +88,50 @@ test('posting bad data -> 422 + UnprocessableEntityError', function(t) {
 
 
 test('should be able to subscribe to indra channel', function (t) {
-	t.plan(1)
+  t.plan(1)
 
-	var aUniqueKey = JSON.stringify(new Date())
+  var aUniqueKey = JSON.stringify(new Date())
 
-	indraChannel.bind('testData', function(d) {
-		receivedUniqueKey = d.data.uniqueKey
-		if (receivedUniqueKey  === aUniqueKey) {
-			t.equal(receivedUniqueKey, aUniqueKey, 'we sent data and received it through pusher')
-		}
-	})
+  indraChannel.bind('testData', function(d) {
+    receivedUniqueKey = d.data.uniqueKey
+    if (receivedUniqueKey  === aUniqueKey) {
+      t.equal(receivedUniqueKey, aUniqueKey, 'we sent data and received it through pusher')
+    }
+  })
 
-	// send a message with our unique key
-	jsonRestClient.post(
-		'/', 
-		{
-			type: 'testData',
-			data: { uniqueKey: aUniqueKey }
-		}, function(error, response, body) { })
+  // send a message with our unique key
+  jsonRestClient.post(
+    '/', 
+    {
+      type: 'testData',
+      data: { uniqueKey: aUniqueKey }
+    }, function(error, response, body) { })
 })
 
 // 
 // TESTS: couch DB tests
 // 
 test('db should exist', function (t) {
-	t.plan(2)
-	db.exists(function (err, exists) {
-		t.equal(true, exists, 'db exists')
-		t.equal(null, err, 'no errors')
-	})
+  t.plan(2)
+  db.exists(function (err, exists) {
+    t.equal(true, exists, 'db exists')
+    t.equal(null, err, 'no errors')
+  })
 })
 
 test('items in couchdb should have createdAt and type fields', function (t) {
-	t.plan(3)
-	db.view('indra-views/byType', { key: 'testData' }, function (err, res) {
-		t.ok(_.first(res), 'at least one (type:testData) post exists')
-		t.ok(_.first(res).value.createdAt, 'createdAt exists')
-		t.ok(_.first(res).value.type, 'type is ok')
-	})
+  t.plan(3)
+  db.view('indra-views/byType', { key: 'testData' }, function (err, res) {
+    t.ok(_.first(res), 'at least one (type:testData) post exists')
+    t.ok(_.first(res).value.createdAt, 'createdAt exists')
+    t.ok(_.first(res).value.type, 'type is ok')
+  })
 })
 
 test('db client can query by createdAt', function (t) {
-	t.plan(1)
-	db.view('indra-views/byCreatedAt', { }, function (err, res) {
-		t.ok(_.first(res), 'at least one post exists from createdAt key {}')
-	})
+  t.plan(1)
+  db.view('indra-views/byCreatedAt', { }, function (err, res) {
+    t.ok(_.first(res), 'at least one post exists from createdAt key {}')
+  })
 })
 

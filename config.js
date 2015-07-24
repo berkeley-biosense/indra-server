01ac96ae8d919aa11f03c968a5214f3df7b55c47
elsehow <ffff@localhost.(none)>
2015-07-23 22:37:07 -0700

    added confi.gjs

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	PORT: 3000
+	, LOG_NAME: 'debug.log'
+	, UNPROCESSABLE_ENTITY_ERROR_MESSAGE: 'Could not process what you posted - probably bad headers (not application/json) or a bad schema (posted data needs to have a field type, which contains a string).'
+}
